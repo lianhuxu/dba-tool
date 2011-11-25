612fd5a1c872d8e0ad0b69b0b092575bbe3c3ab5
Zhou Zhenxing <orczhou@gmail.com>
2011-11-25 13:45:38 +0800

    add some copy right info

--- tbdba-restore-mysqldump.pl
+++ tbdba-restore-mysqldump.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/perl
+
+# First written by orczhou.com orchzou@gmail.com
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 2 of the License.
+
 use strict;
 use File::stat;     # To get the file stat
 use Time::localtime;
